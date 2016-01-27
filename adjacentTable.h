47fa4eb53d67dce52ffa49cb446053213d9e8245
Wiliam Chen <leobuzhi@gmail.com>
2016-01-27 22:52:46 +0800

    Update adjacentTable.h

--- adjacentTable.h
+++ adjacentTable.h
@@ -5,12 +5,23 @@ class AdjacentTable {
 public:
 	AdjacentTable();
 	~AdjacentTable();
-
+	void		setValue(int value,int pos);
+	void		insertVertex(int value);
+	void		insertEdgeByPos(int v1,int v2,char weight);
+	void		insertEdgeByValue(int value1, int value2, char weight);
+	void		removeAllEdges();
+	void		clear();
+	void		outPutNFA();
+	int*		closure(int* t);
+	int*		move(int* t, char ch);
+	int		getPosByValue(int value) const;
+	int		getValueByPos(int pos) const;
+	char		getWeightByValue(int v1,int v2) const;
+	char		getWeightByPos(int value1, int value2) const;
 
 private:
+	Vertex*		startVertex_;
+	int		vertexNum_;
+	int		edgeNum_;
 };
-
-
-
-
 #endif 
