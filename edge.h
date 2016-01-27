8781ab42e67696a1e37ad38b305d45e480d3ab7b
Wiliam Chen <leobuzhi@gmail.com>
2016-01-27 22:50:56 +0800

    Update edge.h

--- edge.h
+++ edge.h
@@ -3,14 +3,13 @@
 class Edge {
 public:
 	Edge();
+	Edge(int number,int position,char weight);
 	~Edge();
 
 private:
-
+	int		number_;
+	int		position_;
+	char		weight_;
+	Edge*		link_;
 };
-
-
-
-
-
 #endif 
