19a2606da9be12c100f7557347f9ed533166341c
Wiliam Chen <leobuzhi@gmail.com>
2016-01-27 22:54:29 +0800

    Update dfa.h

--- dfa.h
+++ dfa.h
@@ -6,11 +6,34 @@ class DFA {
 public:
 	DFA();
 	~DFA();
+	void			inputRegex();
+	void			insertNode();
+	void			regexToPost();
+	void			match();
+	void			inputString();
+	void			setDFAStates();
+	void			getEdgeNumber();
+	void			thompson();
+	void			NFAToDFA();
+	void			hopcroft();
+	void			getAcceptState();
+	int			precedence(char symbol);
+	int			judgeVertex(char ch);
 private:
-
+	char*			regex_;
+	char*			regexPost_;
+	char*			edgeNum_;
+	int			edgeNumber_;
+	std::string		input_;
+	std::string     	matchout_;
+	int			NFAStatesNumber_;
+	int			DFAStatesNumber_;
+	int			NFAAcceptStates[500];
+	int			DFAAcceptStates[500];
+	int			NFANodeAll[100][100];
+	AdjacentTable*		DFATable;
+	AdjacentTable*		NFATable;
+	char			DFAStates[101][101];
+	int			miniDFAStates[101][101];
 };
-
-
-
-
 #endif 
