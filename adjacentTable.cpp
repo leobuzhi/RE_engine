db836805ab7e035118fe68ae25b24e15fc999ea7
Wiliam Chen <leobuzhi@gmail.com>
2016-01-28 22:39:21 +0800

    Create adjacentTable.cpp

---
+++ adjacentTable.cpp
@@ -0,0 +1,61 @@
+#include"adjacentTable.h"
+
+AdjacentTable::AdjacentTable()
+{
+
+}
+
+AdjacentTable::~AdjacentTable()
+{
+
+}
+
+int AdjacentTable::getValueByPos(int position) const 
+{
+
+}
+
+int AdjacentTable::getPosByValue(int value) const
+{
+
+}
+
+char AdjacentTable::getWeightByPos(int value1,int value2) const
+{
+
+}
+
+char AdjacentTable::getWeightByValue(int v1,int v2)	const
+{
+
+}
+
+void AdjacentTable::setValue(int value,int position) 
+{
+
+}
+
+void AdjacentTable::insertVertex(int value)
+{
+
+}
+
+void AdjacentTable::insertEdgeByPos(int v1, int v2, char weight)
+{
+
+}
+
+void AdjacentTable::insertEdgeByValue(int value1, int value2, char weight)
+{
+
+}
+
+void AdjacentTable::removeAllEdges()
+{
+
+}
+
+void AdjacentTable::clear()
+{
+
+}
