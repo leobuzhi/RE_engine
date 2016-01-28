c15df7fc63a60c9ed3982ef6311369497a672ab1
Wiliam Chen <leobuzhi@gmail.com>
2016-01-28 22:39:48 +0800

    Update adjacentTable.h

--- adjacentTable.h
+++ adjacentTable.h
@@ -11,9 +11,6 @@ public:
 	void		insertEdgeByValue(int value1, int value2, char weight);
 	void		removeAllEdges();
 	void		clear();
-	void		outPutNFA();
-	int*		closure(int* t);
-	int*		move(int* t, char ch);
 	int		getPosByValue(int value) const;
 	int		getValueByPos(int pos) const;
 	char		getWeightByValue(int v1,int v2) const;
