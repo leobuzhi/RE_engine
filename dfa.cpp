4e43e49d24e9b3c7e0cedf0cc601bb0da62cf74f
Wiliam Chen <leobuzhi@gmail.com>
2016-01-30 22:25:21 +0800

    Update dfa.cpp

--- dfa.cpp
+++ dfa.cpp
@@ -3,7 +3,14 @@
 #include<iostream>
 DFA::DFA()
 {
-
+	regex_ = new char[256];
+	regexPost_ = new char[256];
+	edgeNum_ = new char[256];
+	edgeNumber_ = 0;
+	DFAStatesNumber_ = 0;
+	NFAStatesNumber_ = 0;
+	NFATable = new AdjacentTable();
+	DFATable = new AdjacentTable();
 }
 
 DFA::~DFA()
