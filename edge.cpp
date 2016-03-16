816f96efad9bf2b12d70449ae5e286060b7747dd
Wiliam Chen <leobuzhi@gmail.com>
2016-03-16 20:52:03 +0800

    Update edge.cpp

--- edge.cpp
+++ edge.cpp
@@ -2,15 +2,15 @@
 
 Edge::Edge()
 {
-	number_ = -1;
-	position_ = -1;
+	position1_ = -1;
+	position2_ = -1;
 	link_ = nullptr;
 }
 
 Edge::Edge(int number, int position, char ch)
 {
-	number_ = number;
-	position_ = position;
+	position1_ = number;
+	position2_ = position;
 	weight_ = ch;
 	link_ = nullptr;
 }
