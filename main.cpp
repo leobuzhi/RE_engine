5d68c09a2350fd8cc74acbb1ca6334d04ed3dbe1
Wiliam Chen <leobuzhi@gmail.com>
2016-03-16 21:06:34 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,20 @@
 #include "dfa.h"
 int main()
 {
-	DFA dfa_test;
-	dfa_test.inputRegex();
-	dfa_test.insertNode();
-	dfa_test.regexToPost();
-	dfa_test.getEdgeNumber();
-	dfa_test.thompson();
-	dfa_test.inputString();
-	dfa_test.match();
+	std::cout << "input n :" << std::endl;
+	int n;
+	std::cin >> n;
+	while (n--)
+	{
+		DFA dfa_test;
+		dfa_test.inputRegex();
+		dfa_test.insertNode();
+		dfa_test.regexToPost();
+		dfa_test.getEdgeNumber();
+		dfa_test.thompson();
+		dfa_test.NFAToDFA();
+		dfa_test.inputString();
+		dfa_test.match();
+	}
 	return 0;
 }
